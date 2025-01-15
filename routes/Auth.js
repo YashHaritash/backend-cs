4257ca5eda8d979a6d3c495de489088fff874bcd
YashHaritash <yashharitash4@gmail.com>
2025-01-15 20:03:27 +0530

    Auth routes code refactoring

--- routes/Auth.js
+++ routes/Auth.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 
 const SECRET = "yashisagoodboy";
 
-//no login required
+// no login required
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -13,18 +13,18 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ email });
     if (!user) return res.status(404).send("User not found");
     if (!user.comparePassword(password))
-      return res.status(401).send("Incorrect password");
+      return res.status(401).json("Incorrect password");
 
     jwt.sign({ id: user._id }, SECRET, (err, token) => {
-      if (err) return res.status(500).send("Internal server error");
-      res.send(token);
+      if (err) return res.status(500).json("Internal server error");
+      res.json(token);
     });
   } catch (err) {
-    return res.status(500).send("Internal server error");
+    return res.status(500).json({ message: "Internal server error" }); // Fix here
   }
 });
 
-//no login required
+// no login required
 router.post("/signup", async (req, res) => {
   try {
     const { email, password, name } = req.body;
